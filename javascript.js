5a76eb4ddab023ec5a450f1efcb0df605e9ade23
sbj-215 <125727832+sbj-215@users.noreply.github.com>
2023-02-18 21:58:10 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -23,87 +23,87 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("tunes/w.wav");
+      var tom1 = new Audio("w.wav");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("tunes/a.wav");
+      var tom2 = new Audio("a.wav");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('tunes/s.wav');
+      var tom3 = new Audio('s.wav');
       tom3.play();
       break;
 
     case "d":
-      var tom4 = new Audio('tunes/d.wav');
+      var tom4 = new Audio('d.wav');
       tom4.play();
       break;
 
     case "j":
-      var snare = new Audio('tunes/j.wav');
+      var snare = new Audio('j.wav');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('tunes/k.wav');
+      var crash = new Audio('k.wav');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('tunes/l.wav');
+      var kick = new Audio('l.wav');
       kick.play();
       break;
     
       case "e":
-        var e = new Audio('tunes/e.wav');
+        var e = new Audio('e.wav');
         e.play();
         break;
   
       case "f":
-        var f = new Audio('tunes/f.wav');
+        var f = new Audio('f.wav');
         f.play();
         break;
   
       case "t":
-        var t = new Audio('tunes/t.wav');
+        var t = new Audio('t.wav');
         t.play();
         break;
   
       case "y":
-        var y = new Audio('tunes/y.wav');
+        var y = new Audio('y.wav');
         y.play();
         break;
   
       case "u":
-        var u = new Audio('tunes/u.wav');
+        var u = new Audio('u.wav');
         u.play();
         break;
         
         case "o":
-        var o = new Audio('tunes/o.wav');
+        var o = new Audio('o.wav');
         o.play();
         break;
   
       case "p":
-        var p = new Audio('tunes/p.wav');
+        var p = new Audio('p.wav');
         p.play();
         break;
   
       case "g":
-        var g = new Audio('tunes/g.wav');
+        var g = new Audio('g.wav');
         g.play();
         break;
   
       case "h":
-        var h = new Audio('tunes/h.wav');
+        var h = new Audio('h.wav');
         h.play();
         break;
   
       case "m":
-          var m = new Audio('tunes/;.wav');
+          var m = new Audio(';.wav');
           m.play();
           break;
     default: console.log(key);
